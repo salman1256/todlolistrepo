0e25b912ff927ad1a9176822e045085afccedf7e
salman1256@yahoo.com <salman1256@yahoo.com>
2026-02-07 12:57:23 +0800

    Added delete task features

--- todolist.py
+++ todolist.py
@@ -2,3 +2,9 @@ tasks=['Planning & Analysis','Design','Coding']
 new_task=input('Enter task name to add in list')
 tasks.append(new_task)
 print('Tasks: ',tasks)
+
+delete_task=input('Enter task to delete')
+if delete_task in tasks:
+    tasks.remove(delete_task)
+
+print('Tasks:',tasks)
